551a4a2ac46fb15c2367ae32c6a4a4683889c10e
Tevin Campbell <tevincampbell73@yahoo.com>
2024-11-24 12:47:48 -0500

    made navbar component whole

--- solarite/src/components/Navbar.jsx
+++ solarite/src/components/Navbar.jsx
@@ -1,8 +1,24 @@
 import { navList } from "@/constants/constants";
 import { Link } from "react-router-dom";
+import solariteLogo from "/solarite-logo-w.svg";
+import { Button } from "@/components/Button";
+import { useNavigate } from "react-router-dom";
 
 export function Navbar() {
+  const navigate = useNavigate();
+  const gotToPath = (obj) => {
+    navigate(obj.path, { state: obj.state });
+  };
     return(
+
+      <header className="flex justify-between w-full items-center z-10 text-white p-5 fixed backdrop-blur-[5px]">
+      <Link to="/">
+        <img
+          src={solariteLogo}
+          alt="solarite logo"
+          className="w-h-14 h-14"
+        />
+      </Link>
         <nav className="flex gap-14">
         {navList.map((nav) => (
           <Link
@@ -14,5 +30,12 @@ export function Navbar() {
           </Link>
         ))}
       </nav>
+      <Button
+        onClick={() => gotToPath({ path: "/login", state: "login" })}
+        label="Login"
+        className="py-2 text-black transition-all duration-500 bg-white rounded-full px-9 hover:bg-transparent hover:border hover:text-white"
+      />
+    </header>
+
     )
 }
