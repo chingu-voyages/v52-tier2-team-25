7ec92d8b0bf5b606394351e97003335c0fa74cf0
Tevin Campbell <tevincampbell73@yahoo.com>
2024-11-30 12:10:30 -0500

    styled tables some more

--- solarite/src/pages/UserPage/ui/Table.jsx
+++ solarite/src/pages/UserPage/ui/Table.jsx
@@ -3,12 +3,17 @@ import { SecondaryButton } from "@/components/Secondarybutton"
 export function Table () {
     return(
         <div id="table">
-            <table className="w-full h-full">
+            <table className="w-[60rem] h-full border border-sky-100 rounded">
             <SecondaryButton label="Create appointment +"/>
-                <tr>
-                    <th className="text-white">Appt</th>
-                    <th className="text-white">Date</th>
-                    <th className="text-white">Time</th>
+                <tr className="border border-sky-100">
+                    <th className="text-white border border-sky-100">Appt</th>
+                    <th className="text-white border border-sky-100">Date</th>
+                    <th className="text-white border border-sky-100">Time</th>
+                </tr>
+                <tr className="border border-sky-200">
+                    <td className="text-white p-2 text-center border border-sky-200">solar removal</td>
+                    <td className="text-white p-2 text-center border border-sky-200">Nov.30, 2024</td>
+                    <td className="text-white p-2 text-center border border-sky-200">12.00 PM</td>
                 </tr>
             </table>
         </div>
