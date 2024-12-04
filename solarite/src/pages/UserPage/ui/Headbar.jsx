92892bac3470fa365b487551a55c735c830ca429
Deborah Lyra <deborahp.lyra@gmail.com>
2024-12-04 14:50:25 -0300

    added: sign out button

--- solarite/src/pages/UserPage/ui/Headbar.jsx
+++ solarite/src/pages/UserPage/ui/Headbar.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 import solariteLogo from "/solarite-logo-w.svg";
 import { FaUserCircle } from "react-icons/fa";
 import { RiArrowDropDownLine } from "react-icons/ri";
+import { useLoginForm } from "@/hooks/useLoginForm";
 
 
 
 export function HeadBar() {
 
-    return(
-      <header className="flex justify-between w-full items-center z-10 text-white p-5 fixed backdrop-blur-[5px]">
+  const { handleSignOut } = useLoginForm({ setUser: () => { } });
+  return (
+    <header className="flex justify-between w-full items-center z-10 text-white p-5 fixed backdrop-blur-[5px]">
       <Link to="/userPage">
         <img
           src={solariteLogo}
@@ -17,11 +19,18 @@ export function HeadBar() {
         />
       </Link>
 
-      <div id="user--account" className="flex gap-2">
-        <FaUserCircle className="text-[1.3em]"/>
-        <RiArrowDropDownLine className="text-[1.5em] cursor-pointer"/>
+      <div className="flex">
+        <div id="user--account" className="flex gap-2">
+          <FaUserCircle className="text-[1.3em]" />
+          <RiArrowDropDownLine className="text-[1.5em] cursor-pointer" />
+        </div>
+        <button
+          className="text-white bg-transparent"
+          label="Logout"
+          onClick={handleSignOut}
+        >Sign out</button>
       </div>
     </header>
 
-    )
+  )
 }
