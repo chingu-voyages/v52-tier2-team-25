9e6e8ce0a60632ea65a26a082b923537a793158f
Yash <yashwanth.venkatesan@gmail.com>
2024-12-15 10:53:02 -0500

    added gh-pages and updated configs

--- solarite/vite.config.js
+++ solarite/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/v52-tier2-team-25/",
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
