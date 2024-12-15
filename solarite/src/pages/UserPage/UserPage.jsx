3e42483a8c23b8d6ec6e29ef4be58e8bd3f5bb3d
Tevin Campbell <tevincampbell73@yahoo.com>
2024-12-15 03:16:43 -0500

    rm: toast

--- solarite/src/pages/UserPage/UserPage.jsx
+++ solarite/src/pages/UserPage/UserPage.jsx
@@ -1,18 +1,14 @@
 import { AppointmentSetter } from "./ui/AppointmentSetter";
 import { HeadBar } from "./ui/Headbar";
-import { Toast } from "./ui/Toast";
-import { useState } from "react";
 
 export function UserPage() {
-  const [toastMessage, setToastMessage] = useState(null)
   return (
     <div className="w-screen h-screen flex flex-col relative overflow-x-hidden">
       <div>
         <HeadBar />
       </div>
-      <div id="content" className="flex items-center justify-center w-full h-full">
+      <div id="content" className="flex items-center justify-center w-full py-12">
         <AppointmentSetter />
-        <Toast message={toastMessage} onClose={() => setToastMessage(null)}/>
       </div>
     </div>
   );
